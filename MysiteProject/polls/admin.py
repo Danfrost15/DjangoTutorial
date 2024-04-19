ee2a807ecabe2db6afa4b81a1bdca19e94ccab80
Dhulkiflu Garba <dhuliflugarba@gmail.com>
2024-04-19 23:35:48 +0100

    Adjusted the admin page

--- MysiteProject/polls/admin.py
+++ MysiteProject/polls/admin.py
@@ -1,6 +1,18 @@
 from django.contrib import admin
-from .models import Question
-from .models import Students_RegNo
+from .models import Question, Choice,Students_RegNo
 
-admin.site.register(Question)
 admin.site.register(Students_RegNo)
+admin.site(Choice)
+
+class ChoiceInline(admin.StackedInline):
+    model = Choice
+    extra = 3
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {"fields": ["question_text"]}),
+        ("Date information", {"fields": ["pub_date"], "classes": ["collapse"]}),
+    ]
+    inlines = [ChoiceInline]
+admin.site.register(Question,QuestionAdmin)
+
